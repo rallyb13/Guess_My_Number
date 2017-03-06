3fb6114b07ce87ce5335cc2d1f71cf72539ff76a
Benjamin Herson <benjaminherson@gmail.com>
2017-03-06 15:32:09 -0800

    init project, create core scenes with scene-to-scene nav buttons

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel (string name) {
+		Debug.Log ("Level Load Requested for: " + name);
+		Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest() {
+		Debug.Log ("Quit Request Called");
+		Application.Quit();
+	}
+}
